89ae53f137d83a5898f56750844b6aaf0fadfe1d
Vinh Phan <vinhphanh@yahoo.com>
2020-03-04 16:32:40 -0500

    Change app text to 'Welcome to East Bay'

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/*Edit <code>src/App.js</code> and save to reload.*/}
-          Hello there!
+          Welcome to East Bay!
         </p>
         <a
           className="App-link"
